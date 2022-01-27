377d257f8fb0bb591f64dfd93940fa16c938c00d
Silvio Marques Ferreira <silvio.ferreira@kantaribopemedia.com>
2022-01-27 01:22:32 -0300

    Seção 3: introdução ao EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,41 +13,22 @@ namespace ProEventos.API.Controllers
     [Route("api/evento")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
+            this._context = context;
         }
-
-        public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento() {
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote  = "1º Lote",
-                QtdPessoas  = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
-                ImagemURL = "foto.png"
-            },
-            new Evento() {
-                EventoId = 2,
-                Tema = "Angular e suas novidades",
-                Local = "São Paulo",
-                Lote  = "2º Lote",
-                QtdPessoas  = 300,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/mm/yyyy"),
-                ImagemURL = "foto1.png"
-            }
-        };
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(x=>x.EventoId == id);
+            return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
         }
         [HttpPost]
         public string Post()
