df75f3dd89f4cba58bed9fa48ed5e812ed824b0b
Silvio Marques Ferreira <silvio.ferreira@kantaribopemedia.com>
2022-01-27 00:05:59 -0300

    Seção 2: DotNet 5 New

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/evento")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento() {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Belo Horizonte",
+                Lote  = "1º Lote",
+                QtdPessoas  = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
+                ImagemURL = "foto.png"
+            },
+            new Evento() {
+                EventoId = 2,
+                Tema = "Angular e suas novidades",
+                Local = "São Paulo",
+                Lote  = "2º Lote",
+                QtdPessoas  = 300,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/mm/yyyy"),
+                ImagemURL = "foto1.png"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(x=>x.EventoId == id);
+        }
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de Put com id = {id}";
+        }
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo de Delete com id = {id}";
+        }
+    }
+}
